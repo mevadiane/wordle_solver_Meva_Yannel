03417e1cb1afacf7ef8665239d39f95c2de424c2
mevadiane <mevarabeharinoro@gmail.com>
2025-04-24 13:20:53 +0400

    Resolution v2

--- include/getBDD.h
+++ include/getBDD.h
@@ -2,5 +2,5 @@
 #define TOTAL_WORDS 2309
 #define MAX_WORD_LENGTH 6
 void getWord(char* bddName, char words[TOTAL_WORDS][MAX_WORD_LENGTH];
-void filter_contain(char liste[TOTAL_WORDS][MAX_WORD_LENGTH], char* lettre)
-void filter_exclude(char liste[TOTAL_WORDS][MAX_WORD_LENGTH], char* lettre)
+void filter_contain(char liste[TOTAL_WORDS][MAX_WORD_LENGTH], char* lettre);
+void filter_exclude(char liste[TOTAL_WORDS][MAX_WORD_LENGTH], char* lettre);
